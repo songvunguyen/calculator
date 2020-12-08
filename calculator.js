c70e3a60af16932626e0a048fc016d7a62eee8ce
Song Vu Nguyen <vunnems08@gmail.com>
2020-12-08 12:15:52 -0500

    Add most of the function. Need to debug

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@
 let x = undefined;
 let y = undefined;
 let op = "";
+let display = "";
 
 function add(a,b){
     return a+b;
@@ -37,3 +38,66 @@ function operate(op, a, b){
             return "Error";
     }
 }
+
+function clearScreen(){
+    let equation = document.querySelector("#equation");
+    let result = document.querySelector("#result");
+    equation.textContent = "";
+    result.textContent = "";
+    
+    //reset all global variables
+    x = undefined;
+    y = undefined;
+    op = "";
+}
+
+function screenDisplay(content){
+    if(x != undefined && y != undefined && op != ""){
+        return;
+    }
+
+    let equation = document.querySelector("#equation");
+
+    if(content === "+" || content === "-" || content === "*" || content === "/"){
+            x = parseInt(equation.textContent);
+            op = content;
+            equation.textContent += " " + content + " ";
+    }else{
+        y = parseInt(content);
+        equation.textContent += content;
+    }
+    
+}
+
+function calculate(){
+    return operate(op, x, y);
+}
+
+let clear = document.querySelector("#C");
+clear.addEventListener('click',() => {
+    clearScreen();
+});
+
+let digits = document.querySelectorAll(".digits");
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => {
+        screenDisplay(digit.textContent);
+    });
+});
+
+let operands = document.querySelectorAll(".operand");
+operands.forEach((operand) => {
+    operand.addEventListener('click', () => {
+        screenDisplay(operand.textContent);
+    });
+});
+
+let equal = document.querySelector("#equal");
+equal.addEventListener('click',() => {
+    let result = document.querySelector("#result");
+    let num = calculate();
+    console.log(x);
+    console.log(y);
+    console.log(op);
+    result.textContent = num.toString();
+});
